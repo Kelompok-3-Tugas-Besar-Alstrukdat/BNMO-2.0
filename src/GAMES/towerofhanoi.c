a5d18400ef1bdc8b4710b0ac2ed0ad4494e9c296
khairunnisahi <117323237+khairunnisahi@users.noreply.github.com>
2022-12-01 18:38:42 +0700

    Update towerofhanoi.c

--- src/GAMES/towerofhanoi.c
+++ src/GAMES/towerofhanoi.c
@@ -5,6 +5,16 @@
 
 void TowerofHanoi()
 {
+    CreateEmptyStack(*tiangA);
+    CreateEmptyStack(*tiangB);
+    CreateEmptyStack(*tiangC);
+
+    Push(*tiangA, '*********');
+    Push(*tiangA, ' ******* ');
+    Push(*tiangA, '  *****  ');
+    Push(*tiangA, '   ***   ');
+    Push(*tiangA, '    *    ');
+    
     ArrayDin Tiang = MakeArrayDin();
     Tiang.Neff = 3;
     int i;
@@ -36,34 +46,36 @@ void TowerofHanoi()
     
 /*asal = tiang asal inputan user*/
 /*tujuan = tiang tujuan inputan user*/
-    
-    if (isWordEqual(asal, Tiang.Elmt[0]){
-        if (isWordEqual(tujuan, Tiang.Elmt[1]){
-            push(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
-            pop(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
+    while (/*nentuin stacknya dah stack buat menang belom*/){
+        if (isWordEqual(asal, Tiang.Elmt[0])){
+            if (isWordEqual(tujuan, Tiang.Elmt[1])){
+                push(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
+                pop(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
+            }
+            else if (isWordEqual(tujuan, Game.Elmt[2])){
+                push(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
+                pop(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
+            }
         }
-        else if (isWordEqual(tujuan, Game.Elmt[2])){
-            push(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
-            pop(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
+        else if (isWordEqual(asal, Tiang.Elmt[1])){
+            if (isWordEqual(tujuan, Tiang.Elmt[0])){
+                push(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
+                pop(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
+            }
+            else if (isWordEqual(tujuan, Game.Elmt[2])){
+                push(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
+                pop(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
+            }
+        }
+        else if (isWordEqual(asal, Tiang.Elmt[2])){
+            if (isWordEqual(tujuan, Tiang.Elmt[0])){
+                push(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
+                pop(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
+            }
+            else if (isWordEqual(tujuan, Game.Elmt[1])){
+                push(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
+                pop(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
+            }
         }
     }
-    else if (isWordEqual(asal, Tiang.Elmt[1]){
-        if (isWordEqual(tujuan, Tiang.Elmt[0])){
-            push(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
-            pop(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
-        }
-        else if (isWordEqual(tujuan, Game.Elmt[2])){
-            push(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
-            pop(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
-        }
-    }
-    else if (isWordEqual(asal, Tiang.Elmt[2]){
-        if (isWordEqual(tujuan, Tiang.Elmt[0])){
-            push(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
-            pop(Tiang.Elmt[0], info(TOP(Tiang.Elmt[0])));
-        }
-        else if (isWordEqual(tujuan, Game.Elmt[1])){
-            push(Tiang.Elmt[2], info(TOP(Tiang.Elmt[2])));
-            pop(Tiang.Elmt[1], info(TOP(Tiang.Elmt[1])));
-        }
-    }
+}
